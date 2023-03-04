22cb22f92e8e1c411caa31610bf6461357a01240
Nicholas Campanelli <ncampanelli0@gmail.com>
2023-03-03 19:25:16 -0500

    working example of localstorage
    file under pages/test/Blazoredlocalstoragetest.razor

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Blazored.SessionStorage;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddBlazoredSessionStorage();
+builder.Services.AddBlazoredLocalStorage();
 
 
 var app = builder.Build();
