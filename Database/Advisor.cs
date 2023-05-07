265d710deadacc7efb0f3df109148e38d83534ad
Nicholas Campanelli <ncampanelli0@gmail.com>
2023-05-06 21:01:19 -0400

    finished sanity checks

--- Database/Advisor.cs
+++ Database/Advisor.cs
@@ -11,26 +11,36 @@ namespace CollegeWebsite2.Database
 
         [BsonElement("FirstName")]
         public string? FirstName {get; set;} = null;
+
         [BsonElement("LastName")]
         public string? LastName {get; set;} = null;
+
         [BsonElement("Birthday")]
         public string? Birthday {get; set;} = null;
+
         [BsonElement("Email")]
         public string? Email {get; set;} = null;
+
         [BsonElement("Password")]
         public string? Password {get; set;} = null;
+
         [BsonElement("AccountType")]
         public string? AccountType {get; set;} = null;
+
         [BsonElement("Address")]
         public string? Address {get; set;} = null;
+
         [BsonElement("LoginCounter")]
         public int? LoginCounter { get; set; } = null;
+
         [BsonElement("PartorFullTime")]
         public string? PartOrFullTime {get; set;} = null;
+
         [BsonElement("PhoneNumber")]
         public string? PhoneNumber {get; set;} = null;
 
-        
+        [BsonElement("UserID")]
+        public string? UserID { get; set; } = null;
 
     }
 }
