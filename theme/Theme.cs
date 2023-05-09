2daaf303ff45f863abfd65275e525903b61a3d77
Nicholas Campanelli <ncampanelli0@gmail.com>
2023-05-08 21:22:19 -0400

    finished converting bootstrap to mudblazor, removed all jinja and added Theme.cs

---
+++ theme/Theme.cs
@@ -0,0 +1,13 @@
+﻿    public class Theme
+    {
+        public string textFieldColor { get; set; }
+        
+        public Theme()
+        {
+            textFieldColor = 
+            "color: #637cf7;";
+
+            
+        }
+    }
+
